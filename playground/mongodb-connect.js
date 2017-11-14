5e86d463d8a90781ce5ee5980de770e26c25a063
JonathanWang <wangjonathan1992@gmail.com>
2017-11-14 17:16:18 -0500

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,32 @@
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+  if (err) {
+    return console.log("Unable to connect to MongoDB server");
+  }
+  console.log("c=Connected to MongoDB server");
+
+  // db.collection("Todos").insertOne({
+  //   text: "Something to do",
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //     return console.log("Unable to insert Todo", err);
+  //   }
+
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection("User").insertOne({
+    name: "Jonathan",
+    age: 24,
+    location: "Boston"
+  }, (err, result) => {
+    if (err) {
+      return console.log("Unable to insert user");
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,38 @@
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+  if (err) {
+    return console.log("Unable to connect to MongoDB server");
+  }
+  console.log("Connected to MongoDB server");
+  //   db
+  //     .collection("Todos")
+  //     .find({completed: false})
+  //     .toArray()
+  //     .then(
+  //       docs => {
+  //         console.log(JSON.stringify(docs, undefined, 2));
+  //       },
+  //       err => {
+  //         console.log("Unable to fetch todos", err);
+  //       }
+  //     );
+  //   db
+  //     .collection("Todos")
+  //     .find()
+  //     .count()
+  //     .then(
+  //       count => {
+  //         console.log(`Todos count: ${count}`);
+  //       },
+  //       err => {
+  //         console.log("Unable to fetch todos", err);
+  //       }
+  //     );
+  db.collection("Users").find({ name: "Jonathan" }).toArray().then(docs => {
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, err => {
+    console.log("Unable to fetch todos", err);
+  });
+  //   db.close();
+});
