b3720609aedb92df0ce39ab0f3f6b74f6adefeba
JonathanWang <wangjonathan1992@gmail.com>
2017-11-15 15:43:40 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var { ObjectID } = require("mongodb");
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./model/todo");
 var { User } = require("./model/user");
@@ -34,6 +35,26 @@ app.get("/todos", (req, res) => {
     }
   );
 });
+
+app.get("/todos/:id", (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+  Todo.findById(id).then(
+    todo => {
+      if (!todo) {
+        res.status(400).send();
+      }
+
+      res.send({ todo });
+    },
+    e => {
+      res.status(400).send();
+    }
+  );
+});
+
 app.listen(3000, () => {
   console.log("Started on port 3000");
 });
